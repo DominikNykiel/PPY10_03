20ef3dd37c224f34571fe8617248e0ead00dc4b0
DominikNykiel <s24813@pjwstk.edu.pl>
2023-03-17 16:16:20 +0100

    Add script for WaybackMachine site seeking, add keyboard input

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
-# This is a sample Python script.
+
 
 import webbrowser
 import requests
 
+
+#Zad4
 #pageUrl = 'https://github.com'
 #date = 20150101
 
 pageUrl = input("podaj adres strony")
-date = input("podaj date np. 20230130")
-url = 'http://archive.org/wayback/available?url=' + pageUrl + '&timestamp' + str(date)
-response = requests.get(url)
-d = response.json()
-print(d)
-page = d["archived_snapshots"]["closest"]["url"]
 
-webbrowser.open(pageUrl)
+print("Podaj trzy daty, z których wybrać snapshoty")
+for x in range(3):
+    date = input("podaj date np. 20230130")
+
+    url = "http://archive.org/wayback/available?url=" + pageUrl + "&timestamp=" + str(date)
+    response = requests.get(url)
+    d = response.json()
+    print(d)
+    page = d["archived_snapshots"]["closest"]["url"]
+    webbrowser.open(page)
+print("Trzy strony znalezione!")
+
+
