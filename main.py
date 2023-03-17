180fdf6911a04395f2970b1a19f6dba2bcc7f0b0
DominikNykiel <s24813@pjwstk.edu.pl>
2023-03-17 21:06:44 +0100

    Add script for sound playing using VLC and playsound, improve website search

--- main.py
+++ main.py
@@ -1,25 +1,34 @@
 
-
 import webbrowser
 import requests
+from playsound import playsound
+import vlc
+import time
 
+# zad3
 
-#Zad4
-#pageUrl = 'https://github.com'
-#date = 20150101
+# Za pomocą playsound
+# playsound("D:\PycharmProjects\PPY10_03\yippee.mp3")
+##I za pomocą vlc
+# p = vlc.MediaPlayer("D:\PycharmProjects\PPY10_03\yippee.mp3")
+# p.play()
+# time.sleep(3)
+# p.stop()
 
-pageUrl = input("podaj adres strony")
+# Zad4
+pageUrl = 'https://github.com'
+date = 20150101
 
-print("Podaj trzy daty, z których wybrać snapshoty")
+pageUrl = input("podaj adres strony\n")
+
+print("Podaj trzy daty, z których wybrać snapshoty\n")
 for x in range(3):
-    date = input("podaj date np. 20230130")
+    date = input("podaj date np. 20230130\n")
 
     url = "http://archive.org/wayback/available?url=" + pageUrl + "&timestamp=" + str(date)
     response = requests.get(url)
     d = response.json()
-    print(d)
+    print(d ,"\n")
     page = d["archived_snapshots"]["closest"]["url"]
     webbrowser.open(page)
 print("Trzy strony znalezione!")
-
-
